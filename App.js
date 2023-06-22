abb7ceb709b62172b33b2a4b0ae26a9c3a9a604a
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-06-22 12:51:47 +0530

    Added Inline Style

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
@@ -7,9 +7,10 @@ export default function App() {
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
       <View>
-        <Text style={{margin: 16,borderColor:'red'}}>Hello World!</Text>
+        <Text style={{ margin: 16, borderColor: 'red' }}>Hello World 1!</Text>
+        <Text style={styles.dummyText}>Hello World 2!</Text>
       </View>
-      <Button title='Click Me!'/>
+      <Button title="Click Me!" />
     </View>
   );
 }
@@ -17,8 +18,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  dummyText: {
+    margin: 16,
+    padding: 16,
+    borderWidth: 2,
+    borderColor: "red",
   },
 });
