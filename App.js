506d7bc5291da22b14e080f8441a384f2b676271
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-06-22 12:49:17 +0530

    Added Inline Style and StyleSheet Object

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View>
+        <Text style={{margin: 16,borderColor:'red'}}>Hello World!</Text>
+      </View>
+      <Button title='Click Me!'/>
     </View>
   );
 }
