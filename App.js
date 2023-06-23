a5a2aa359d05be96ddf0de301dafce9d927da34c
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-06-23 08:26:11 +0530

    Remove Functions,States, and StyleSheet in the App.js

--- App.js
+++ App.js
@@ -1,39 +1,7 @@
-// import { StatusBar } from "expo-status-bar";
-import { useState } from "react";
-import {
-  Button,
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-  FlatList,
-} from "react-native";
-import GoalItem from "./components/GoalItem";
-import GoalInput from "./components/GoalInput";
 import CategoryScreen from "./screens/CategoryScreen";
 
 export default function App() {
-  const [courseGoals, setCourseGoals] = useState([]);
-
-  const addGoalHandler = (enteredGoalText) => {
-    setCourseGoals((currentCourseGoals) => [
-      ...currentCourseGoals,
-      { text: enteredGoalText, id: Math.random().toString() },
-    ]);
-  };
-
   return (
    <CategoryScreen />
   );
 }
-
-const styles = StyleSheet.create({
-  appContainer: {
-    flex: 1,
-    paddingTop: 50,
-    paddingHorizontal: 16,
-  },
-  goalContainer: {
-    flex: 4,
-  },
-});
