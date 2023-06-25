a357244106d2a353b12cd95ce323499cd699e370
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-06-25 21:20:49 +0530

    Added Styles in the iconButton.js

--- components/iconButton.js
+++ components/iconButton.js
@@ -1,12 +1,22 @@
-import { Pressable } from "react-native";
+import { Pressable, StyleSheet } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 
-const IconButton = ({onPress}) => {
-return (
-    <Pressable onPress={onPress}>
-         <Ionicons name="ios-star-outline" size={24} color="white" />
+const IconButton = ({ onPress }) => {
+  return (
+    // object destructuring use karala pressed kiyana variable eka ganna puluwan mekedi palaveni eka true nm dewani eka return karannawa.
+    <Pressable
+      onPress={onPress}
+      style={({ pressed }) => pressed && styles.pressed}
+    >
+      <Ionicons name="ios-star-outline" size={24} color="white" />
     </Pressable>
-);
+  );
 };
+
+const styles = StyleSheet.create({
+  pressed: {
+    opacity: 0.7,
+  },
+});
 
 export default IconButton;
