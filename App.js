b2be8e5d3f78f9eea862e2d1f71e06b51084547d
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-06-25 22:03:25 +0530

    Refactored @react-navigation/drawer and added screenOptions

--- App.js
+++ App.js
@@ -1,12 +1,33 @@
+import React from "react";
 import { StatusBar } from "expo-status-bar";
-import CategoryScreen from "./screens/CategoryScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+
+import CategoryScreen from "./screens/CategoryScreen";
 import MealsOverviewScreen from "./screens/MealsOverviewScreen";
 import MealDetailScreen from "./screens/MealDetailScreen";
-import { Button } from "react-native";
+import FavoritesScreen from "./screens/FavoritesScreen";
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+//start drawer navigator
+const DrawerNavigator = () => {
+  return (
+    <Drawer.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: "#351401" },
+        headerTintColor: "#fff",
+        sceneContainerStyle: { backgroundColor: "#3f2f25" },
+      }}
+    >
+      <Drawer.Screen name="Categories" component={CategoryScreen} />
+      <Drawer.Screen name="Favorites" component={FavoritesScreen} />
+    </Drawer.Navigator>
+  );
+};
+//end drawer navigator
 
 export default function App() {
   return (
@@ -16,7 +37,7 @@ export default function App() {
         <Stack.Navigator
           initialRouteName="Meals Categories"
           screenOptions={{
-            headerStyle: { backgroundColor: "#3514" },
+            headerStyle: { backgroundColor: "#351401" },
             headerTintColor: "#fff",
             headerTitleStyle: { fontWeight: "bold" },
             contentStyle: { backgroundColor: "#3f2f25" },
@@ -25,8 +46,8 @@ export default function App() {
           {/* default ganna screen eka thamai udinma thina eka, nattam apita initialRouteName kiyala default ganna ona eka dennath puluvan*/}
           <Stack.Screen
             name="MealsCategories"
-            component={CategoryScreen}
-            options={{ title: "Meals Categories" }}
+            component={DrawerNavigator}
+            options={{ headerShown: false }}
           />
           <Stack.Screen
             name="MealsOverview"
@@ -36,12 +57,14 @@ export default function App() {
           <Stack.Screen
             name="MealDetail"
             component={MealDetailScreen}
-            options={{
-              // title: "Meal Details",
-              // headerRight: () => (
-              //   <Button title="Tap Me!" />
-              // ),
-            }}
+            options={
+              {
+                // title: "Meal Details",
+                // headerRight: () => (
+                //   <Button title="Tap Me!" />
+                // ),
+              }
+            }
           />
         </Stack.Navigator>
       </NavigationContainer>
