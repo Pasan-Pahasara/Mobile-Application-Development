5f9a5ad17c14efbc5b66303065aa47128c808c3c
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-06-26 11:13:17 +0530

    FullyRefactored Added Meals to the favorite list using Redux

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -2,14 +2,23 @@ import { useContext } from "react";
 import { Text, View, StyleSheet } from "react-native";
 import { FavoriteContext } from "../store/context/favorite-context";
 import { MEALS } from "../data/dummy-data";
+import { useSelector } from 'react-redux'
+
 import MealsList from "../components/MealList/MealsList";
 
 const FavoritesScreen = (props) => {
-  const favoriteMealsCtx = useContext(FavoriteContext);
+//   const favoriteMealsCtx = useContext(FavoriteContext); //using context api
+
+const favoriteMealsIds = useSelector((state) => state.favoriteMeals.ids) //redux eke state eka useSelector eken gannawa
+
+
+//   const favoriteMeals = MEALS.filter((meal) =>
+//     favoriteMealsCtx.ids.includes(meal.id)
+//   );
 
   const favoriteMeals = MEALS.filter((meal) =>
-    favoriteMealsCtx.ids.includes(meal.id)
-  );
+  favoriteMealsIds.includes(meal.id)
+);
 
   if (favoriteMeals.length === 0 || !favoriteMeals) {
     return (
